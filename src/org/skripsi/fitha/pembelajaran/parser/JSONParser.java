243663441c794f0a4be002f441ae491bf009fe22
nauin <adnantoa@gmail.com>
2015-04-18 11:14:17 +0700

    Penambahan kelas java baru yang digunakan untuk melakukan parser terhadap json

--- src/org/skripsi/fitha/pembelajaran/parser/JSONParser.java
+++ src/org/skripsi/fitha/pembelajaran/parser/JSONParser.java
@@ -1,22 +1,18 @@
 package org.skripsi.fitha.pembelajaran.parser;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.util.Log;
@@ -32,16 +28,12 @@ public class JSONParser {
 	public JSONObject ambilJSonUrl(String url) {
 		try {
 			DefaultHttpClient httClient = new DefaultHttpClient();
-			HttpPost httpPost = new HttpPost(json);
+			HttpPost httpPost = new HttpPost(url);
 			HttpResponse httResponse = httClient.execute(httpPost);
 			HttpEntity httpEntity = httResponse.getEntity();
 			is = httpEntity.getContent();
-		} catch (UnsupportedEncodingException uee) {
-			uee.printStackTrace();
-		} catch (ClientProtocolException cpe) {
-			cpe.printStackTrace();
-		} catch (IOException ioe) {
-			ioe.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 		
 		try {
@@ -60,8 +52,8 @@ public class JSONParser {
 		
 		try {
 			jObj = new JSONObject(json);
-		} catch (JSONException jse) {
-			Log.e("JSON Parser", "Error parsing data " + jse.toString());
+		} catch (Exception e) {
+			Log.e("JSON Parser", "Error parsing data " + e.toString());
 		}
 		
 		return jObj;
@@ -88,12 +80,8 @@ public class JSONParser {
 				HttpEntity httpEntity = httpResponse.getEntity();
 				is = httpEntity.getContent();
 			}
-		} catch (UnsupportedEncodingException uee) {
-			uee.printStackTrace();
-		} catch (ClientProtocolException cpe) {
-			cpe.printStackTrace();
-		} catch (IOException ioe) {
-			ioe.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 
 		try {
@@ -114,8 +102,8 @@ public class JSONParser {
 
 		try {
 			jObj = new JSONObject(json);
-		} catch (JSONException je) {
-			Log.e("JSON Parser", "Error parsing data " + je.toString());
+		} catch (Exception e) {
+			Log.e("JSON Parser", "Error parsing data " + e.toString());
 		}
 
 		return jObj;
