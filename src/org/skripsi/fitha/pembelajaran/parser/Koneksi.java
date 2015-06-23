90d8fab6c73d7f2fad1e0afbd253a934591160b5
Adnanto A.R <adnantoa@gmail.com>
2015-06-23 14:20:34 +0700

    Perubahan pada kelas Koneksi

--- src/org/skripsi/fitha/pembelajaran/parser/Koneksi.java
+++ src/org/skripsi/fitha/pembelajaran/parser/Koneksi.java
@@ -5,7 +5,7 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
 public class Koneksi {
-	private String urlLearning = null;
+	private String urlLearning = "http://elearningmath.nazuka.net/";
 
 	public Koneksi(String url) {
 		urlLearning = url;
