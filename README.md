82d3f584486244d2a4b1d056eb59f1ac80fd8686
Adnanto A.R <adnantoa@gmail.com>
2015-07-11 20:50:08 +0700

    Dikarenakan sebuah alasan, berkas banyak yang dihapus, dan diganti dengan yang baru

--- README.md
+++
@@ -1,2 +0,0 @@
-# athif-narajalebmep
-narajalebmep
--- gen/org/skripsi/fitha/pembelajaran/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package org.skripsi.fitha.pembelajaran;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
